4341769b54f9f1a7d81e2573650c1dcbbb530c83
Unknown <adtsolanki22@gmail.com>
2022-09-15 19:30:12 +0530

    User Logout Page

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,11 +1,14 @@
 import Link from 'next/link';
-import { useSession } from 'next-auth/react'
+import { useSession, signOut} from 'next-auth/react'
 import classes from './main-navigation.module.css';
 
 function MainNavigation() {
    const { data: session, status } = useSession();
    console.log("loading",status);
    console.log("session",session);
+   const handleLogout = () => {
+      signOut();
+   }
   return (
     <header className={classes.header}>
       <Link href='/'>
@@ -28,7 +31,7 @@ function MainNavigation() {
           {/* if session is there than show logout */}
           {session && (
             <li>
-               <button>Logout</button>
+               <button onClick={handleLogout}>Logout</button>
             </li>
           )}
         </ul>
