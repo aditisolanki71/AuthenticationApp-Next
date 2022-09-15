4ab8be29829d62aaf82315810b8c611196fa0579
Unknown <adtsolanki22@gmail.com>
2022-09-15 18:00:21 +0530

    Adding the "Credentials Auth Provider" & user login logic

--- helper/auth.js
+++ helper/auth.js
@@ -1,6 +1,13 @@
-import { hash } from "bcryptjs";
+import { hash, compare} from "bcryptjs";
 export async function hashPassword(password) {
    //original plain text as password
    const hashedPassword = await hash(password,12);
    return hashedPassword;
 }
+//find if plain text pwd is same as hash pwd or not?
+export async function verifyPassword(password,hashedPassword) {
+   //original plain text as password
+   const isValid = await compare(password,hashedPassword);
+   return isValid;
+}
+
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,31 @@
+import NextAuth from "next/auth";
+import Providers from "next-auth/providers";
+import { connectToDatabase } from "../../../helper/db";
+import { verifyPassword } from "../../../helper/auth"
+export default NextAuth({
+   session: { 
+      jwt: true
+    },
+   providers: [
+      Providers.Crendentials({
+         //whn rcv incoming req
+         async authorize(credentials) {
+            const client = await connectToDatabase();
+            const usersCollections = client.db().collection("users");
+            const user = await usersCollections.findOne({ email: credentials.email });
+            if(!user) {
+               client.close();
+               throw new Error("No user Found!!!");
+            }
+            const isValidPwd = await verifyPassword(credentials.password,user.password);
+            if(!isValidPwd) {
+               client.close();
+               throw new Error("Could not log you in!!!");
+            } 
+            client.close();
+            return { email: user.email };
+           
+         }
+      })
+   ]
+});
