ec238df4758988bbfabbb88d0d56b975525197f5
Unknown <adtsolanki22@gmail.com>
2022-09-16 11:42:08 +0530

    Adding server-side page guards

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,29 +1,31 @@
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
-import { 
-   //useSession, 
-   getSession} from 'next-auth/react'
-import { useState, useEffect } from "react";
+// client side code
+// import { 
+//    //useSession, 
+//    getSession} from 'next-auth/react'
+// import { useState, useEffect } from "react";
 function UserProfile() {
-  // Redirect away if NOT auth
-  const [isLoading,setIsLoading] = useState(true);
-  useEffect(() => {
-     getSession().then(session => {
-           if(!session) {
-               window.location.href = '/auth';
-           }
-           else {
-               setIsLoading(false);
-           }
-      });
-  },[])
-//   const { data: session, status } = useSession();
-//   if(status === 'authenticated') {
-//      return <p className={classes.profile}>Loading....</p>
-//   }
-   if(isLoading) {
-      return <p className={classes.profile}>Loading....</p>
-   }
+   // client side code
+//   // Redirect away if NOT auth
+//   const [isLoading,setIsLoading] = useState(true);
+//   useEffect(() => {
+//      getSession().then(session => {
+//            if(!session) {
+//                window.location.href = '/auth';
+//            }
+//            else {
+//                setIsLoading(false);
+//            }
+//       });
+//   },[])
+// //   const { data: session, status } = useSession();
+// //   if(status === 'authenticated') {
+// //      return <p className={classes.profile}>Loading....</p>
+// //   }
+//    if(isLoading) {
+//       return <p className={classes.profile}>Loading....</p>
+//    }
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,23 @@
 import UserProfile from '../components/profile/user-profile';
-
+import { getSession } from 'next-auth/react'
 function ProfilePage() {
+  //user profile component will only be render if the ppage renders
+  //page render will only happen if we are authenticated user coz of serversideprops logic
   return <UserProfile />;
 }
-
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req })
+  if(!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        //only this time when not vali session
+        permanent: false
+      }
+    }
+  }
+  return {
+    props: { session },
+  }
+}
 export default ProfilePage;
